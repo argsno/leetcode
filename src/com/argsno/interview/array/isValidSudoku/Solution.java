b4a9956c2410203c6e4d7d22e077e64fa7c1458e
黄志强 <huangzhiqiang02@meituan.com>
2019-02-11 14:37:20 +0800

    array

---
+++ src/com/argsno/interview/array/isValidSudoku/Solution.java
@@ -0,0 +1,89 @@
+package com.argsno.interview.array.isValidSudoku;
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        return validRowAndCol(board) && validGrid(board);
+    }
+
+    private boolean validRowAndCol(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            boolean[] m1 = new boolean[9];
+            boolean[] m2 = new boolean[9];
+            for (int j = 0; j < 9; j++) {
+                if (valid(board, i, j, m1)) {
+                    return false;
+                }
+                if (valid(board, j, i, m2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean validGrid(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                boolean[] m = new boolean[9];
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        int row = i * 3 + k;
+                        int col = j * 3 + l;
+                        if (valid(board, row, col, m)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean valid(char[][] board, int i, int j, boolean[] m) {
+        if (board[i][j] != '.') {
+            int num = board[i][j] - '1';
+            if (m[num]) {
+                return true;
+            }
+            m[num] = true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isValidSudoku(new char[][]{
+                {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        }));
+        System.out.println(solution.isValidSudoku(new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        }));
+        System.out.println(solution.isValidSudoku(new char[][]{
+                {'.', '.', '4', '.', '.', '.', '6', '3', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'5', '.', '.', '.', '.', '.', '.', '9', '.'},
+                {'.', '.', '.', '5', '6', '.', '.', '.', '.'},
+                {'4', '.', '3', '.', '.', '.', '.', '.', '1'},
+                {'.', '.', '.', '7', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '5', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'}
+        }));
+    }
+}
---
+++ src/com/argsno/interview/array/moveZeroes/Solution.java
@@ -0,0 +1,34 @@
+package com.argsno.interview.array.moveZeroes;
+
+import java.util.Arrays;
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0, right;
+        while (left < nums.length) {
+            if (nums[left] == 0) {
+                break;
+            }
+            left++;
+        }
+        right = left + 1;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                nums[right] = 0;
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] in1 = new int[]{0, 1, 0, 3, 12};
+        solution.moveZeroes(in1);
+        System.out.println(Arrays.toString(in1));
+        int[] in2 = new int[]{0};
+        solution.moveZeroes(in2);
+        System.out.println(Arrays.toString(in2));
+    }
+}
