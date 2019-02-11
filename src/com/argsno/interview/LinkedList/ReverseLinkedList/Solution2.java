d87f40d51f27ed74195c1b1cc05815eb5170bd92
黄志强 <huangzhiqiang02@meituan.com>
2019-02-11 18:52:28 +0800

    LinkedList

---
+++ src/com/argsno/interview/LinkedList/ReverseLinkedList/Solution2.java
@@ -0,0 +1,18 @@
+package com.argsno.interview.LinkedList.ReverseLinkedList;
+
+import com.argsno.ListNode;
+
+public class Solution2 {
+    public ListNode reverseList(ListNode head) {
+        return reverseList(head, null);
+    }
+
+    public ListNode reverseList(ListNode cur, ListNode prev) {
+        if (cur == null) {
+            return prev;
+        }
+        ListNode next = cur.next;
+        cur.next = prev;
+        return reverseList(next, cur);
+    }
+}
