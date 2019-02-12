063384eae2ab8ccdd2e6647721a6d3f07b6e1d2c
黄志强 <huangzhiqiang02@meituan.com>
2019-02-12 11:16:28 +0800

    DynamicProgramming

---
+++ src/com/argsno/interview/DynamicProgramming/ClimbingStairs/Solution.java
@@ -0,0 +1,26 @@
+package com.argsno.interview.DynamicProgramming.ClimbingStairs;
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int prev = 1;
+        int next = 2;
+        int cur;
+        while (n-- > 2) {
+            cur = prev + next;
+            prev = next;
+            next = cur;
+        }
+        return next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(5));
+    }
+}
---
+++ src/com/argsno/interview/DynamicProgramming/MaximumSubarray/Solution.java
@@ -0,0 +1,27 @@
+package com.argsno.interview.DynamicProgramming.MaximumSubarray;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
+        for (int num : nums) {
+            cur += num;
+            if (cur > max) {
+                max = cur;
+            }
+            if (cur < 0) {
+                cur = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(solution.maxSubArray(new int[]{Integer.MIN_VALUE}));
+    }
+}
